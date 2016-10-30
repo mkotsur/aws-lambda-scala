afdff6a5b4b99f76853b386dfc3bf73de177a1f6
Mike Kotsur <miccots@gmail.com>
2016-10-30 17:14:20 +0100

    The test is green!

--- src/main/scala/io/github/mkotsur/HelloWorldHandler.scala
+++ src/main/scala/io/github/mkotsur/HelloWorldHandler.scala
@@ -1,9 +1,16 @@
 package io.github.mkotsur
 
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
 import java.io.{InputStream, OutputStream}
+import java.nio.charset.Charset
 
 import com.amazonaws.services.lambda.runtime.Context
 import io.github.mkotsur.model.{Ping, Pong}
+
+import scala.io.Source
 
 
 class HelloWorldHandler {
@@ -11,10 +18,10 @@ class HelloWorldHandler {
   def handle(ping: Ping): Pong = Pong(ping.msg.reverse)
 
   def handleInternal(input: InputStream, output: OutputStream, ctx: Context = null): Unit = {
-    // convert `input` into Pong
-    // apply `handle()`
-    // convert the result into JSON
-    // write that into `output`
+    decode[Ping](Source.fromInputStream(input).mkString)
+      .map(handle)
+      .map(_.asJson.noSpaces)
+      .foreach(jsonStrong => output.write(jsonStrong.getBytes(Charset.defaultCharset())))
   }
 
 }
