91160196684ef95975bc8c2e98fcb9457f73b898
Mike Kotsur <miccots@gmail.com>
2016-10-30 17:04:22 +0100

    Test and steps

--- src/main/scala/io/github/mkotsur/HelloWorldHandler.scala
+++ src/main/scala/io/github/mkotsur/HelloWorldHandler.scala
@@ -1,9 +1,20 @@
 package io.github.mkotsur
 
+import java.io.{InputStream, OutputStream}
+
+import com.amazonaws.services.lambda.runtime.Context
 import io.github.mkotsur.model.{Ping, Pong}
+
 
 class HelloWorldHandler {
 
   def handle(ping: Ping): Pong = Pong(ping.msg.reverse)
+
+  def handleInternal(input: InputStream, output: OutputStream, ctx: Context = null): Unit = {
+    // convert `input` into Pong
+    // apply `handle()`
+    // convert the result into JSON
+    // write that into `output`
+  }
 
 }
---
+++ src/test/scala/io/github/mkotsur/HelloWorldHandlerTest.scala
@@ -0,0 +1,21 @@
+package io.github.mkotsur
+
+import java.io.ByteArrayOutputStream
+
+import com.amazonaws.util.StringInputStream
+import org.scalatest._
+
+class HelloWorldHandlerTest extends FunSuite with ShouldMatchers {
+
+  test("testHandle") {
+
+    val input = """{ "msg": "hello" }"""
+
+    val baos: ByteArrayOutputStream = new ByteArrayOutputStream()
+
+    new HelloWorldHandler().handleInternal(new StringInputStream(input), baos)
+
+    baos.toString shouldBe """{"msg":"olleh"}"""
+  }
+
+}
