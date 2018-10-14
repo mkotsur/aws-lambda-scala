a4a8824b832ffdd5afccc4c759de451b9dfe8fdf
Mike Kotsur <miccots@gmail.com>
2018-10-14 22:04:29 +0200

    scalafmt

--- src/main/scala/io/github/mkotsur/aws/handler/Lambda.scala
+++ src/main/scala/io/github/mkotsur/aws/handler/Lambda.scala
@@ -84,39 +84,40 @@ object Lambda {
     }
   }
 
-  implicit def canEncodeFuture[I](implicit canEncode: Encoder[I]) = CanEncode.instance[Future[I]]((os, responseEither, ctx) => {
-    for {
-      response <- responseEither
-      futureResult <- Try(Await.result(response, ctx.getRemainingTimeInMillis millis)).toEither
-      json <- Try(canEncode(futureResult).noSpaces.getBytes).toEither
-      _ <- Try(os.write(json)).toEither
-    } yield {
-      ()
-    }
-  })
+  implicit def canEncodeFuture[I](implicit canEncode: Encoder[I]) =
+    CanEncode.instance[Future[I]]((os, responseEither, ctx) => {
+      for {
+        response     <- responseEither
+        futureResult <- Try(Await.result(response, ctx.getRemainingTimeInMillis millis)).toEither
+        json         <- Try(canEncode(futureResult).noSpaces.getBytes).toEither
+        _            <- Try(os.write(json)).toEither
+      } yield {
+        ()
+      }
+    })
 
   implicit def canEncodeProxyResponse[T](implicit canEncode: CanEncode[T]) = CanEncode.instance[ProxyResponse[T]](
     (output, proxyResponseEither, ctx) => {
 
-      def writeBody(bodyOption: Option[T]): Either[Throwable, Option[String]] = {
+      def writeBody(bodyOption: Option[T]): Either[Throwable, Option[String]] =
         bodyOption match {
           case None => Right(None)
           case Some(body) =>
-            val os = new ByteArrayOutputStream()
+            val os     = new ByteArrayOutputStream()
             val result = canEncode.writeStream(os, Right(body), ctx)
             os.close()
             result.map(_ => Some(os.toString()))
         }
-      }
 
       val proxyResposeOrError = for {
         proxyResponse <- proxyResponseEither
-        bodyOption <- writeBody(proxyResponse.body)
-      } yield ProxyResponse[String](
-        proxyResponse.statusCode,
-        proxyResponse.headers,
-        bodyOption
-      )
+        bodyOption    <- writeBody(proxyResponse.body)
+      } yield
+        ProxyResponse[String](
+          proxyResponse.statusCode,
+          proxyResponse.headers,
+          bodyOption
+        )
 
       val response = proxyResposeOrError match {
         case Right(proxyRespose) =>
